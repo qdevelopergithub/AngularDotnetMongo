c71e150ff49059c24a166f9aa7cf0c02242ae277
QDev <114226099+gurjeetsingh622001@users.noreply.github.com>
2024-10-08 11:08:47 +0530

    backend

---
+++ Dotnet/Program.cs
@@ -0,0 +1,45 @@
+using CrudWithMongoDB.DataAccess;
+using CrudWithMongoDB.EncryptionHelper;
+using CrudWithMongoDB.Middleware;
+using MongoDB.Driver;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<MongoDbServices>();
+builder.Services.AddTransient<EncryptionHelper>();
+builder.Services.AddScoped<EncryptionHelper>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+//app.UseMiddleware<EncryptionMiddleWare>();
+
+app.UseHttpsRedirection();
+
+
+app.UseCors("AllowAllOrigins");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
